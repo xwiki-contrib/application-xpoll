20dce6efb126de652d2cb3d1c98d54958c9d13a1
Gabriela Smeria <gabriela.smeria@xwiki.com>
2015-03-30 13:18:53 +0300

    [Misc] Functional Tests * Fixed indentation * Added a test for the creation of a new poll while setting the status 'Active'

--- application-xpoll-test/application-xpoll-test-pageobjects/src/main/java/org/xwiki/xpoll/test/po/XPollHomePage.java
+++ application-xpoll-test/application-xpoll-test-pageobjects/src/main/java/org/xwiki/xpoll/test/po/XPollHomePage.java
@@ -5,52 +5,52 @@ import org.openqa.selenium.support.FindBy;
 import org.xwiki.test.ui.po.ViewPage;
 
 /**
- *
  * @version $Id$
  * @since 1.6.4
  */
 
-public class XPollHomePage extends ViewPage{
-	
-	@FindBy(xpath = "//div[@id = 'actionBox']//a[@class= 'action add']")
-	private WebElement addNewEntryButton;
-	
-	@FindBy(xpath = "//div[@id = 'entryNamePopup']//input[@type = 'text']")
-	private WebElement addPollEntryInput;
-	
-	@FindBy(xpath = "//div[@id = 'entryNamePopup']//input[@type = 'image']")
-	private WebElement addPollEntryButton;
-	
-	public static XPollHomePage gotoPage()
-	{
-		getUtil().gotoPage(getSpace(), getPage());
-		return new XPollHomePage();
-	}
-	
-	public static String getPage()
-	{
-		return "WebHome";
-	}
-	
-	public static String getSpace()
-	{
-		return "XPoll";
-	}
-	
-	public void clickAddNewEntryButton()
-	{
-		addNewEntryButton.click();
-	}
-	
-	public void setAddPollEntryInput(String pollName)
-	{
-		this.addPollEntryInput.clear();
-		this.addPollEntryInput.sendKeys(pollName);
-	}
-	
-	public XPollEditPage clickAddPollEntryButton()
-	{
-		this.addPollEntryButton.click();
-		return new XPollEditPage();
-	}
+public class XPollHomePage extends ViewPage
+{
+
+    @FindBy(xpath = "//div[@id = 'actionBox']//a[@class= 'action add']")
+    private WebElement addNewEntryButton;
+
+    @FindBy(xpath = "//div[@id = 'entryNamePopup']//input[@type = 'text']")
+    private WebElement addPollEntryInput;
+
+    @FindBy(xpath = "//div[@id = 'entryNamePopup']//input[@type = 'image']")
+    private WebElement addPollEntryButton;
+
+    public static XPollHomePage gotoPage()
+    {
+        getUtil().gotoPage(getSpace(), getPage());
+        return new XPollHomePage();
+    }
+
+    public static String getPage()
+    {
+        return "WebHome";
+    }
+
+    public static String getSpace()
+    {
+        return "XPoll";
+    }
+
+    public void clickAddNewEntryButton()
+    {
+        addNewEntryButton.click();
+    }
+
+    public void setAddPollEntryInput(String pollName)
+    {
+        this.addPollEntryInput.clear();
+        this.addPollEntryInput.sendKeys(pollName);
+    }
+
+    public XPollEditPage clickAddPollEntryButton()
+    {
+        this.addPollEntryButton.click();
+        return new XPollEditPage();
+    }
 }
--- application-xpoll-test/application-xpoll-test-tests/src/test/it/org/xwiki/xpoll/test/ui/XPollTest.java
+++ application-xpoll-test/application-xpoll-test-tests/src/test/it/org/xwiki/xpoll/test/ui/XPollTest.java
@@ -1,59 +1,88 @@
 package org.xwiki.xpoll.test.ui;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
+import org.xwiki.panels.test.po.ApplicationsPanel;
 import org.xwiki.test.ui.AbstractTest;
 import org.xwiki.test.ui.SuperAdminAuthenticationRule;
 import org.xwiki.test.ui.po.ViewPage;
+import org.xwiki.xpoll.test.po.ActiveStatusViewPage;
 import org.xwiki.xpoll.test.po.InPreparationStatusViewPage;
 import org.xwiki.xpoll.test.po.XPollEditPage;
 import org.xwiki.xpoll.test.po.XPollHomePage;
-import org.xwiki.panels.test.po.ApplicationsPanel;
 
 /**
- * 
  * @version $Id$
  * @since 1.6.4
- *
  */
 
 public class XPollTest extends AbstractTest
 {
-	@Rule 
-	public SuperAdminAuthenticationRule authenticationRule = new SuperAdminAuthenticationRule(getUtil(), getDriver());
-	
-	public static final String pollName = "Poll 2";
-	public static final String pollDescription = "Poll 1 Description";
-	public static final String pollProposals = "1,2,3,4,5";
-	
-	@Test
-	public void appEntryRedirectsToHomePage()
-	{
-		ApplicationsPanel applicationPanel = ApplicationsPanel.gotoPage();
-		ViewPage vp = applicationPanel.clickApplication("Polls");
-		Assert.assertEquals(XPollHomePage.getSpace(), vp.getMetaDataValue("space"));
-		Assert.assertEquals(XPollHomePage.getPage(), vp.getMetaDataValue("page"));
-		
-	}
-	
-	@Test
-	public void createNewEntry()
-	{
-		XPollHomePage xpollHomePage = XPollHomePage.gotoPage();
-		xpollHomePage.clickAddNewEntryButton();
-		xpollHomePage.setAddPollEntryInput(pollName);
-		
-		XPollEditPage xpollEditPage = xpollHomePage.clickAddPollEntryButton();
-		Assert.assertEquals(pollName, xpollEditPage.getName());
-		xpollEditPage.setDescription(pollDescription);
-		xpollEditPage.setStatusInPreparation();
-		xpollEditPage.setProposals(pollProposals);
-		xpollEditPage.clickSaveAndView();
-		
-		InPreparationStatusViewPage inPreparationStatusViewPage = new InPreparationStatusViewPage();
-		Assert.assertEquals(pollDescription, inPreparationStatusViewPage.getPollDescription());
-		Assert.assertEquals(xpollEditPage.getStatusInPreparation(), inPreparationStatusViewPage.getPollStatus());
-		
-	}
+    @Rule
+    public SuperAdminAuthenticationRule authenticationRule = new SuperAdminAuthenticationRule(getUtil(), getDriver());
+
+    public static final String pollName = "Poll 1";
+
+    public static final String pollDescription = "Poll 1 Description";
+
+    public static final String pollProposals = "1,2,3,4,5";
+
+    public ArrayList<String> proposals = new ArrayList<String>(Arrays.asList(pollProposals.split(",")));
+
+    @Test
+    public void appEntryRedirectsToHomePage()
+    {
+        ApplicationsPanel applicationPanel = ApplicationsPanel.gotoPage();
+        ViewPage vp = applicationPanel.clickApplication("Polls");
+        Assert.assertEquals(XPollHomePage.getSpace(), vp.getMetaDataValue("space"));
+        Assert.assertEquals(XPollHomePage.getPage(), vp.getMetaDataValue("page"));
+
+    }
+
+    @Test
+    public void createNewEntryWithInPreparationStatus()
+    {
+        XPollHomePage xpollHomePage = XPollHomePage.gotoPage();
+        xpollHomePage.clickAddNewEntryButton();
+        xpollHomePage.setAddPollEntryInput(pollName);
+
+        XPollEditPage xpollEditPage = xpollHomePage.clickAddPollEntryButton();
+        Assert.assertEquals(pollName, xpollEditPage.getName());
+        xpollEditPage.setDescription(pollDescription);
+        xpollEditPage.setStatusInPreparation();
+        xpollEditPage.setProposals(pollProposals);
+        xpollEditPage.clickSaveAndView();
+
+        InPreparationStatusViewPage inPreparationStatusViewPage = new InPreparationStatusViewPage();
+        Assert.assertEquals(pollDescription, inPreparationStatusViewPage.getPollDescription());
+        Assert.assertEquals(xpollEditPage.getStatusInPreparation(), inPreparationStatusViewPage.getPollStatus());
+
+    }
+
+    @Test
+    public void createNewEntryWithActiveStatus()
+    {
+        XPollHomePage xpollHomePage = XPollHomePage.gotoPage();
+        xpollHomePage.clickAddNewEntryButton();
+        xpollHomePage.setAddPollEntryInput(pollName);
+
+        XPollEditPage xpollEditPage = xpollHomePage.clickAddPollEntryButton();
+        Assert.assertEquals(pollName, xpollEditPage.getName());
+        xpollEditPage.setDescription(pollDescription);
+        xpollEditPage.setStatusActive();
+        xpollEditPage.setProposals(pollProposals);
+        xpollEditPage.clickSaveAndView();
+
+        ActiveStatusViewPage activeStatusViewPage = new ActiveStatusViewPage();
+        Assert.assertEquals(pollName, activeStatusViewPage.getDocumentTitle());
+        Assert.assertEquals(pollDescription, activeStatusViewPage.getDescription());
+
+        activeStatusViewPage.getProposals();
+        Assert.assertEquals(proposals, activeStatusViewPage.pollProposals);
+
+    }
 }
